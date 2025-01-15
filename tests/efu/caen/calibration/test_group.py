6f77c65abca89e2cc5744f82ed17d982745e1c2d
Gregory Tucker <gregory.tucker@ess.eu>
2025-01-15 13:30:29 +0100

    [Rem] thresholds from calibration, now in CAEN config files

--- tests/efu/caen/calibration/test_group.py
+++ tests/efu/caen/calibration/test_group.py
@@ -6,7 +6,6 @@ def test_efu_caen_calibration_group_validation():
         'groupindex': 1,
         'intervals': [[0, 0.25], [0.251, 0.5], [0.501, 0.75], [0.751, 1]],
         'polynomials': [[0, 0, 0, 0], [1, 1, 1, 1], [2.1, 2.2, 2., 3.4], [0, 0, -1, 2]],
-        'thresholds': [[0, 10000], [1, 2], [20000, 1<<16], [10, 1<<31]]
     }
     assert validate_json(data, uri='https://ess.eu/efu/caen/calibration/group')
 
@@ -18,7 +17,6 @@ def test_efu_caen_calibration_group_throws_on_repeated_intervals():
         'groupindex': 1,
         'intervals': [[0, 1], [0, 1], [0, 1], [0, 1]],
         'polynomials': [[0, 0, 0, 0], [1, 1, 1, 1], [2.1, 2.2, 2., 3.4], [0, 0, -1, 2]],
-        'thresholds': [[0, 10000], [1, 2], [20000, 1 << 16], [10, 1 << 31]]
     }
     with raises(ValidationError) as ex:
         validate_json(repeated_intervals, uri='https://ess.eu/efu/caen/calibration/group')
@@ -32,8 +30,7 @@ def test_efu_caen_calibration_group_throws_on_missing_values():
     repeated_intervals = {
         'groupindex': 1,
         'intervals': [[0, 1]],
-        'polynomials': [[0, 0, 0]],
-        'thresholds': [[0, 10000]]
+        'polynomials': [[0, 0, 0]]
     }
     with raises(ValidationError) as ex:
         validate_json(repeated_intervals,
@@ -47,9 +44,8 @@ def test_efu_caen_calibration_group_throws_on_disallowed_values():
 
     repeated_intervals = {
         'groupindex': 1,
-        'intervals': [[0, 1]],
+        'intervals': [[0, 10]],
         'polynomials': [[0, 0, 0]],
-        'thresholds': [[0, -1]]
     }
     with raises(ValidationError) as ex:
         validate_json(repeated_intervals,
@@ -62,10 +58,9 @@ def test_efu_caen_calibration_group_throws_on_disallowed_types():
     from jsonschema.exceptions import ValidationError
 
     repeated_intervals = {
-        'groupindex': 1,
+        'groupindex': 3.14159,
         'intervals': [[0, 1]],
         'polynomials': [[0, 0, 0]],
-        'thresholds': [[0, 3.14159]]
     }
     with raises(ValidationError) as ex:
         validate_json(repeated_intervals,
